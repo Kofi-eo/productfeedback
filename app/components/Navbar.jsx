4ebc475e2cb944bf32393304df6fd27cd80256bf
Emmanuel Ofori <slxmtube@gmail.com>
2023-05-09 21:20:28 +0000

    Initial Commit
    -added Navbar & Footer
    -imported google fonts in global css

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react';
+
+const Navbar = () => {
+	return (
+		<div>
+			<div className='text-white'>This is a Navbar</div>
+		</div>
+	);
+};
+
+export default Navbar;
