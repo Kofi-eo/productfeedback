c981f42e2102c8097b7c5d3bb5f976a3a2200fa3
Emmanuel Ofori <slxmtube@gmail.com>
2023-05-11 22:43:52 +0000

    push data to feedbackData

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import FeedbackCard from './components/feedbackCard';
+import fbDataMain from '../public/data.json';
 
 export default function Home() {
 	return (
-		<main className='flex min-h-screen flex-col items-center justify-between p-24 bg-[#F7F8FD]'>
-			<FeedbackCard />
+		<main className='flex min-h-screen flex-col items-center justify-between p-24 bg-[#F7F8FD] gap-8'>
+			{fbDataMain?.productRequests.map((feedback, index) => (
+				<FeedbackCard key={index} data={feedback} />
+			))}
 		</main>
 	);
 }
