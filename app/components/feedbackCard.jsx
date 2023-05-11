4a7c52bfdd64681f00e3f124c754a9b9513e1e95
Emmanuel Ofori <slxmtube@gmail.com>
2023-05-11 20:48:36 +0000

    add Data file and Images

---
+++ app/components/feedbackCard.jsx
@@ -0,0 +1,9 @@
+'use client';
+
+import React from 'react';
+
+const feedbackCard = () => {
+	return <div>feedbackCard</div>;
+};
+
+export default feedbackCard;
--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import feedbackCard from './components/feedbackCard';
 
 export default function Home() {
 	return (
-		<main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
+		<main className='flex min-h-screen flex-col items-center justify-between p-24 bg-slate-400'>
+			<feedbackCard />
+		</main>
 	);
 }
